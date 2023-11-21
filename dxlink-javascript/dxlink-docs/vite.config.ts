f66c6b4b7cc4f7793202fc2975e93cc150c60ce3
Dmitry Petrov <dvpetrov.main@gmail.com>
2023-11-21 19:55:04 +0000

    fix(docs): asset paths should be relative

--- dxlink-javascript/dxlink-docs/vite.config.ts
+++ dxlink-javascript/dxlink-docs/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   server: {
     port: 4200,
     host: 'localhost',
